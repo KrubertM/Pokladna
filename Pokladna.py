c079a23ded063582bf050dd091c8154665df109c
KrubertM <martinkrubert@seznam.cz>
2023-10-05 13:19:43 +0200

    Create Pokladna.py

---
+++ Pokladna.py
@@ -0,0 +1,59 @@
+print("Vítejte v obchodě")
+print("Elektronika, Potraviny, Oblečení")
+print("Napíš odejit pokud bideš chtíit skončit s nakupováním")
+
+run = True
+Zbozi = []
+cena = 0
+
+while run:
+    kde = input("Zadej kam chceš: ")
+    
+    if(kde == "Elektronika"):
+        print("Vítej v elektonice")
+        elektronika = ["Počítač - 10000kč", "Telefon - 8000kč", "Kamera - 12000kč"]
+        print("Vyber jsi věc 1-Počítač, 2-Telefon, 3-Kamera: ")
+        co = input("Zadej zboží: ")
+        Zbozi.append (co)
+        if (co == "Počítač"):
+            cena = cena + 10000
+        if (co =="Telefon"):
+            cena = cena + 8000
+        if( co =="Kamera"):
+            cena = cena + 12000
+
+
+    if(kde == "Potraviny"):
+        print("Vítej v potravinách")
+        potraviny = ["Chelba - 130kč", "Rohlík - 2kč", "Okurka - 50kč"]
+        print("Vyber jsi věc 1-Chleba, 2-Rohlík, 3-Okurka: ")
+        co = input("Zadej zboží: ")
+        Zbozi.append (co)
+        if (co == "Chleba"):
+            cena = cena + 130
+        if (co =="Rohlík"):
+            cena = cena + 2
+        if( co =="Okurka"):
+            cena = cena + 50
+
+
+    if(kde =="Oblečení"):
+        print("Vítej v oblečení")
+        oblečení = ["Tričko - 1000", "Kalhoty - 800kč", "Ponožky - 100kč"]
+        print("Vyber jsi věc 1-Tryčko, 2-Kalhoty, 3-Ponožky: ")
+        co = input("Zadej zboží: ")
+        Zbozi.append (co)
+        if (co == "Tričko"):
+            cena = cena + 1000
+        if (co =="Kalhoty"):
+            cena = cena + 800
+        if( co =="Ponožky"):
+            cena = cena + 100
+
+    if(kde == "Odejít"):
+        break
+
+print("Vaše konečné zboží: ")
+print(*Zbozi)
+print("Vaše konečná cena: ")
+print(cena)
